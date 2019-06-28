ce512ad3e608b5d9947144cc73bbdad31fee6228
meeri <meeri.lembinen@gmail.com>
2019-06-28 16:37:13 +0300

    Add geometry and DDEC6 files to data directory

---
+++ data/EMImTFMS/EMImTFMS_gs.py
@@ -0,0 +1,30 @@
+from ase import *
+from ase.io import read, write
+from ase.parallel import paropen, parprint
+from gpaw import GPAW, setup_paths, FermiDirac, PW
+from gpaw import Mixer, MixerSum, MixerDif
+#from gpaw.eigensolvers import Davidson
+from gpaw.poisson import PoissonSolver
+from ase.units import Bohr
+from gpaw import GPAW, restart
+import os
+from gpaw.xas import XAS
+
+setup_paths.insert(0, './')
+
+sys = read('EMImTFMS.xyz')
+
+sys.center(vacuum=8)
+
+calc1 = GPAW(h=0.16,
+             charge=0,
+             mode="fd",
+	     xc='PBE',
+             txt='EMImTFMS_gs.txt',
+             eigensolver='rmm-diis',
+             )
+sys.set_calculator(calc1)
+e1 = sys.get_potential_energy() + calc1.get_reference_energy()
+rho = sys.calc.get_all_electron_density(gridrefinement=2)
+write('EMImTFMS_density2.cube', sys, data=rho * Bohr**3)
+calc.write('EMImTFMS.gpw')
